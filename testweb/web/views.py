5717b79b085c75cd97598f297f283e39ca6c63c6
Jang” <kookjin0111@gmail.com>
2024-05-23 17:26:38 +0900

    web page

--- testweb/web/views.py
+++ testweb/web/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("web 인덱스 파일입니다.")
+	# return HttpResponse("web 인덱스 파일입니다.")
+	db_list = "데이터베이스에서 자료를 가져왔습니다."
+	return render(request, 'web_index.html', {'data':db_list})
+
