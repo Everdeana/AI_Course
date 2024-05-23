b72d5d477e76cd00eaa4956c10e7e108c982e660
Jang” <kookjin0111@gmail.com>
2024-05-23 17:38:40 +0900

    django db test

--- testweb/web/models.py
+++ testweb/web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Test1(models.Model):
+	title = models.CharField(max_length=100)
+	tel = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.title
--- testweb/web/views.py
+++ testweb/web/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Test1
 
 # Create your views here.
 
 def index(request):
 	# return HttpResponse("web 인덱스 파일입니다.")
-	db_list = "데이터베이스에서 자료를 가져왔습니다."
-	return render(request, 'web_index.html', {'data':db_list})
+	db_list = Test1.objects.all()
+	db_list1 = "데이터1"
+	db_list2 = "데이터2"
+	return render(request, 'web_index.html', {'data':db_list, 'data1':db_list1, 'data2':db_list2})
 
