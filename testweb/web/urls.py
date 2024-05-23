cc31533580d1469e54a6aebd3b0cf7bee771dafa
Jang” <kookjin0111@gmail.com>
2024-05-23 16:52:17 +0900

    test web page

---
+++ testweb/web/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	path('', views.index, name='index') # 비어있으면 실행(ex -> path('/list', ~) -> 리스트 페이지
+	# 아무 것도 없으면 뒷부분 실행
+]
--- testweb/web/views.py
+++ testweb/web/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+	return HttpResponse("web 인덱스 파일입니다.")
