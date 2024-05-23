5c1299b1be028351045723ad0afa936a5667b6f3
Jang” <kookjin0111@gmail.com>
2024-05-23 10:36:19 +0900

    ImageReadTest

--- aicctv/camera_test.py
+++ aicctv/camera_test.py
@@ -3,7 +3,17 @@ import cv2
 print(cv2.__version__)
 
 # 이미지 불러오기
-image = cv2.imread("nesta.jpg", cv2.IMREAD_COLOR)
-cv2.imshow("nesta.jpg", image)
+# image = cv2.imread("nesta.jpg", cv2.IMREAD_COLOR)
+# image = cv2.imread("nesta.jpg", cv2.IMREAD_GRAYSCALE)
+
+# 원본 호출
+org_image = cv2.imread("nesta.jpg", cv2.IMREAD_COLOR)
+cv2.imshow("nesta.jpg", org_image)
+
+# 이미지 가공
+gray_image = cv2.imread("nesta.jpg", cv2.IMREAD_GRAYSCALE)
+cv2.imshow("gray nesta.jpg", gray_image)
+
+
 cv2.waitKey()
-cv2.destroyAllWindows
+cv2.destroyAllWindows()
