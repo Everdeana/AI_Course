ba149ddfe30ec5928ec52081808b7045d7835710
Jang” <kookjin0111@gmail.com>
2024-05-23 10:25:00 +0900

    ImageRead

--- aicctv/camera_test.py
+++ aicctv/camera_test.py
@@ -1,3 +1,9 @@
 # opencv 버전 확인
 import cv2
 print(cv2.__version__)
+
+# 이미지 불러오기
+image = cv2.imread("nesta.jpg", cv2.IMREAD_COLOR)
+cv2.imshow("nesta.jpg", image)
+cv2.waitKey()
+cv2.destroyAllWindows
