6921b27c90f489e43f534fabaaf90f37be0186d0
Jang” <kookjin0111@gmail.com>
2024-05-24 17:40:50 +0900

    fileTest

--- face_dataset/create_dataset.py
+++ face_dataset/create_dataset.py
@@ -1,7 +1,14 @@
 import os
 import pandas as pd
 import shutil
+from datetime import datetime
 
+# 오늘 날짜, 시간 구하기
+nowDate = datetime.today().strftime("%Y%m%d_%H%M%S")
+
+print(nowDate)
+
+os.rename('./dataset', f'./dataset_{nowDate}')
 
 # 데이터 불러오기
 def createFolder(dir):
@@ -14,7 +21,7 @@ def createFolder(dir):
 #원본 디렉토리에 파일 불러오기
 orgPath = "./org_data"
 fileList = os.listdir(orgPath)
-print(fileList)
+# print(fileList)
 
 # Dataset Directory 만들기
 createFolder("./dataset")
@@ -24,7 +31,7 @@ dataset_labels = {}  # Initialize an empty dictionary
 # 파일 생성
 saveFile = open('./dataset/labels.txt', 'w', encoding='utf8')
 
-# 디렉토리 생성
+# 디렉토리 생성 함수
 for file in fileList:
     # print(file)
     # 문장 분리
@@ -46,3 +53,13 @@ for file in fileList:
 print(dataset_labels)
 saveFile.close()
 
+# 원본파일 Dataset에 처리하는 루틴
+#원본 디렉토리에 파일 불러오기
+orgPath = "./org_data"
+fileList = os.listdir(orgPath)
+for dir in fileList:
+    files = os.listdir(orgPath + f'/{dir}')
+    print('*'*100)
+    print(dir)
+    print('*'*100)
+    print(files)
