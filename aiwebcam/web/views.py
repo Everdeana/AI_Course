9aab425f6cdb8e2de296aafc9200947df97e15e9
Jang <kookjin0111@gmail.com>
2024-05-25 00:01:01 +0900

    wed face landmark

--- aiwebcam/web/views.py
+++ aiwebcam/web/views.py
@@ -1,40 +1,84 @@
 from django.shortcuts import render
-from django.http import HttpResponse, StreamingHttpResponse
-
-# OpenCV 라이브러리 불러오기
+from django.http import StreamingHttpResponse
 import cv2
-# import dlib  
-# from functools import wraps  
-# from scipy.spatial import distance  
-# import time  
+import dlib
+from scipy.spatial import distance
+import time
+import os
+from functools import wraps
 
-def video_feed(request):  # stream 함수를 streaming
-	return StreamingHttpResponse(stream(), content_type = 'multipart/x-mixed-replace;boundary=frame') # content_type = 전송 규격
+class BlinkDetector:
+    def __init__(self):
+        self.lastsave = 0
+        self.hog_face_detector = dlib.get_frontal_face_detector()
+        self.current_dir = os.path.dirname(os.path.realpath(__file__))
+        self.model_path = os.path.join(self.current_dir, 'shape_predictor_68_face_landmarks.dat')
+        if not os.path.exists(self.model_path):
+            raise RuntimeError(f"Unable to open {self.model_path}")
+        self.dlib_facelandmark = dlib.shape_predictor(self.model_path)
+        self.close_count = 0
 
-# Create your views here.
+    def calculate_EAR(self, eye):
+        A = distance.euclidean(eye[1], eye[5])
+        B = distance.euclidean(eye[2], eye[4])
+        C = distance.euclidean(eye[0], eye[3])
+        ear_aspect_ratio = (A + B) / (2.0 * C)
+        return ear_aspect_ratio
 
-# 얼굴 인식 처리
+    def counter(func):
+        @wraps(func)
+        def tmp(self, *args, **kwargs):
+            tmp.count += 1
+            time.sleep(0.05)
+            if time.time() - self.lastsave > 5:
+                self.lastsave = time.time()
+                tmp.count = 0
+            return func(self, *args, **kwargs)
+        tmp.count = 0
+        return tmp
 
-# 영상을 보내는 함수
+    @counter
+    def close(self, frame):
+        cv2.putText(frame, "Eyes Closed!!!!!!", (20, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 255), 4)
+
+    def detect_and_draw_landmarks(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.hog_face_detector(gray)
+        for face in faces:
+            x, y, w, h = face.left(), face.top(), face.width(), face.height()
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            face_landmarks = self.dlib_facelandmark(gray, face)
+            for n in range(0, 68):
+                x = face_landmarks.part(n).x
+                y = face_landmarks.part(n).y
+                cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+
+            leftEye = [(face_landmarks.part(n).x, face_landmarks.part(n).y) for n in range(36, 42)]
+            rightEye = [(face_landmarks.part(n).x, face_landmarks.part(n).y) for n in range(42, 48)]
+            left_ear = self.calculate_EAR(leftEye)
+            right_ear = self.calculate_EAR(rightEye)
+            EAR = (left_ear + right_ear) / 2
+            EAR = round(EAR, 2)
+            if EAR < 0.19:
+                self.close(frame)
+        return frame
+
+detector = BlinkDetector()
+
+# 영상 스트림 생성 함수
 def stream():
-	cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = detector.detect_and_draw_landmarks(frame)
+        _, jpeg = cv2.imencode('.jpg', frame)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-	while True:
-		ret, frame = cap.read()
-		if not ret:
-			print("카메라를 인식할 수 없습니다.")
-			break
+def video_feed(request):
+    return StreamingHttpResponse(stream(), content_type='multipart/x-mixed-replace; boundary=frame')
 
-		# 서버로 데이터를 전송
-		# 이미지를 binary WEB에서 전송 가능한 형태
-		image_bytes = cv2.imencode('.jpg', frame)[1].tobytes() # . --> 히든파일
-
-		# 서버로 전송 - yield : 공백일 떄까지 계속 실행
-		yield(b'--frame\r\n'
-		b'Content-type:image/jpeg\r\n\r\n'
-		+ image_bytes + b'r\n')  # b = binary
-
-
-def index(request): # web 함수는 request 필수
-	# return HttpResponse("Web Main Page ")
-	return render(request, 'webcam_main.html')
+def index(request):
+    return render(request, 'webcam_main.html')
