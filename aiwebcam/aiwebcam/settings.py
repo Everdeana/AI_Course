cc8b39d715b37616d368786439387757c53d0858
Jang” <kookjin0111@gmail.com>
2024-05-24 10:49:05 +0900

    Django web setting

--- aiwebcam/aiwebcam/settings.py
+++ aiwebcam/aiwebcam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ik3ex#jv31n5+*k41fo3yc10!$6o*@ut((o^joj&+-nya#$q73
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'web',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
