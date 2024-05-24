e168e914907aee2aea9c08201f2b633d06ddb002
Jang” <kookjin0111@gmail.com>
2024-05-24 11:13:17 +0900

    web test

--- aiwebcam/web/views.py
+++ aiwebcam/web/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request): # web 함수는 request 필수
+	return HttpResponse("Web Main Page ")
