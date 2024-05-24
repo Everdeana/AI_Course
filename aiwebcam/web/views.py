020983a9d4ea0306a9fb06dc2e197b933271827b
Jang” <kookjin0111@gmail.com>
2024-05-24 13:02:14 +0900

    html file

--- aiwebcam/web/views.py
+++ aiwebcam/web/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request): # web 함수는 request 필수
-	return HttpResponse("Web Main Page ")
+	# return HttpResponse("Web Main Page ")
+	return render(request, 'webcam_main.html')
