0422378179de6c0ea6326575b55ab5d0d319c11d
Jang” <kookjin0111@gmail.com>
2024-05-27 16:45:45 +0900

    face recog~

--- face_dataset/faiss_train.py
+++ face_dataset/faiss_train.py
@@ -13,7 +13,6 @@ from PIL import Image # pillow : 이미지 처리 라이브러리
 import faiss
 import face_recognition
 
-
 def read_file(path):
     img_path = []
     # 데이터 가져오기
@@ -69,10 +68,57 @@ train_labels = np.array(
 
 # print(train_labels)
 # 데이터 구조 확인
-print("faceEncode : ", type(faceEncode))
-print("train_labels : ", type(train_labels))
+print("faceEncode : ", type(faceEncode)) # <class 'list'>
+print("train_labels : ", type(train_labels)) # <class 'numpy.ndarray'>
 
+# 인코딩 위한 변환 작업
+faceEncode = np.array(faceEncode, dtype=np.float32)
+print(faceEncode.shape, train_labels.shape) # (102, 128) (102,)
 
+# Vector DB로 저장
+face_index = faiss.IndexFlatL2(128) # faiss 엔진에 방금 위에서 확인한 구조(128)로 저장
+
+# 가장 가까운 위치에 있는 값을 계싼 -> 거리가 멀어지면 unknown처리
+# 벡터 적용
+# 총 5개의 인덱스 검출
+face_index.add(faceEncode)
+
+# 예측하기
+# 얼굴 인식
+test_img = face_recognition.load_image_file('test_img/city.jpg')
+test_face = face_recognition.face_locations(test_img)
+if len(test_face) != 1:
+    print("테스트 얼굴에 1개의 얼굴만 존재해야 합니다.")
+    exit()
     
+# 얼굴만 잘라내기(시계방향)
+top, right, bottom, left  = test_face[0]
+print(top, right, bottom, left)
 
+# 범위 확장
+top -= 20
+right += 20
+bottom += 20
+left -= 20
+
+# 얼굴 부붐만 추출
+face_cut = test_img[top:bottom, left:right]
+
+# Encoding
+test_en = face_recognition.face_encodings(face_cut)[0]
+print(type(test_en))
+
+# numpy로 변환
+test_en = np.array(test_en, dtype=np.float32).reshape(-1, 128) # .reshape(-1, 128) ====> (128) -> (101, 128)
+
+######예측하는 부분##########################################################################################################################
+
+# 예측
+val, result = face_index.search(test_en, k=5) # faiss 엔진에서 test_en에 가장 근접한 5개 추출
+
+# 값 검풀
+label = [train_labels[i] for i in result[0]]
+print(label)
+
+############################################################################################################################################
 # 128bit vector encoding
