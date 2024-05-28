94fa97c7dd6613491b9c015b556098ea2b570a44
Jang” <kookjin0111@gmail.com>
2024-05-28 16:02:47 +0900

    image cont

--- yolov5_program/test.py
+++ yolov5_program/test.py
@@ -3,3 +3,27 @@ import torch
 
 # 모델 불러오기
 model = torch.hub.load("ultralytics/yolov5", "yolov5s")
+
+# 파라미터 미세조정
+model.conf = 0.6
+
+# 이미지 검증(Predict)
+img = "https://www.sporf.com/wp-content/uploads/2023/04/afab2c7c-gettyimages-74058407.jpg"
+
+# 검증
+results = model(img)
+
+results.print()
+
+# 프로그램에서 사용할 수 있도록 정보 받기(화면에 이미지 출력)
+results.show()
+
+# 데이터 구조 형태로 보기
+print(results.pandas().xyxy[0])
+
+# 저장
+results.save('./savedata/milan_result.jpg')
+
+# crop
+results.crop()
+
