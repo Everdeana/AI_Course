75cf49b792df7d159501ca3b7962c6c0cfab80e9
Jang” <kookjin0111@gmail.com>
2024-05-30 17:16:11 +0900

    django test

--- beautyganweb/beautyganweb/settings.py
+++ beautyganweb/beautyganweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$r%reboot!uizyewr^x!k+ten@oe4#i@7m8k%1&o+_js)f&b^w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
