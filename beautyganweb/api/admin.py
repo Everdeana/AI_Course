be44be53b12fd57620ee8f791b5bd0e39d3bf7b8
Jang” <kookjin0111@gmail.com>
2024-06-03 12:02:50 +0900

    client

--- beautyganweb/api/admin.py
+++ beautyganweb/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import api_RefModels
+from .models import RefModels
 
-admin.site.register(api_RefModels)
+admin.site.register(RefModels)
--- beautyganweb/api/views.py
+++ beautyganweb/api/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # 직접 생성한 Model 사용 -> Table 사용
-from .models import api_RefModels
+from .models import RefModels
 
 # Create your views here.
 def api_index(request):
@@ -12,10 +12,12 @@ def api_index(request):
 def api_index1(request):
 	return HttpResponse("ChicBytes API v2.0")
 
+
+@csrf_exempt
 def api_getmodellist(request):
 	# DB
 	# select * from api_refmodels
-	datas = api_RefModels.objects.all()
+	datas = RefModels.objects.all()
 
 	sendData = []
 
@@ -30,4 +32,9 @@ def api_getmodellist(request):
 			"doc" : data.doc,
 			"dates" : data.dates
 		})
+
+	data = {
+		"refModel" : sendData,
+		"code" : 1,
+	}
 	return JsonResponse(sendData, safe = False, json_dumps_params = {'ensure_ascii':False}, status = 200)\
